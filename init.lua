026aba02127cc15c995e632cb769d72e0b862b47
Timo Betcke <timo.betcke@gmail.com>
2022-02-10 22:10:02 +0000

    First working config

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 require "user.options"
 require "user.colorscheme"
+require "user.plugins"
+require "user.lsp"
+require "user.keymaps"
+require "user.cmp"
+require "user.gitsigns"
+require "user.telescope"
+require "user.treesitter"
+
+
 
